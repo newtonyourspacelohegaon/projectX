f3dd66e42a1febee3c53ae66a127f87a5b1b016e
newtonyourspacelohegaon <lohegaonyourspace@gmail.com>
2026-01-30 20:47:15 +0530

    Revert: Restore backend as submodule

--- backend/routes/chatRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { sendMessage, getMessages } = require('../controllers/chatController');
-const { protect } = require('../middleware/authMiddleware');
-
-router.post('/send', protect, sendMessage);
-router.get('/:userId', protect, getMessages);
-
-module.exports = router;
